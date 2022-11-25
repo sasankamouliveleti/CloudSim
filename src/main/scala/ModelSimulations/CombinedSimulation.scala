79a6d724b48e1393288b85fc096e65da329c3fea
Sasanka Mouli Veleti <svelet2@uic.edu>
2022-11-25 16:44:32 -0600

    Added Iaas, Saas, Paas, Combined Simulations

---
+++ src/main/scala/ModelSimulations/CombinedSimulation.scala
@@ -0,0 +1,80 @@
+package ModelSimulations
+
+import HelperUtils.CreateLogger
+import com.typesafe.config.{Config, ConfigFactory}
+import org.cloudbus.cloudsim.brokers.DatacenterBrokerSimple
+import org.cloudbus.cloudsim.cloudlets.Cloudlet
+import org.cloudbus.cloudsim.core.CloudSim
+import org.cloudbus.cloudsim.datacenters.DatacenterSimple
+import org.cloudbus.cloudsim.hosts.Host
+import org.cloudbus.cloudsim.network.topologies.BriteNetworkTopology
+import org.cloudbus.cloudsim.vms.Vm
+import org.cloudsimplus.builders.tables.CloudletsTableBuilder
+import org.slf4j.Logger
+
+import java.util.Comparator
+import scala.collection.immutable.List
+import scala.jdk.CollectionConverters.*
+
+object CombinedSimulation {
+  val logger: Logger = CreateLogger(classOf[CombinedSimulation])
+  val config1: Config = ConfigFactory.load("Iaas.conf").getConfig("Iaas")
+  val config2: Config = ConfigFactory.load("Saas.conf").getConfig("Saas")
+  val config3: Config = ConfigFactory.load("Paas.conf").getConfig("Paas")
+  val mainConfig: Config = ConfigFactory.load("application.conf").getConfig("applicationConfigParams")
+
+  def main(args: Array[String]): Unit = {
+    executeSimulation()
+  }
+
+  def executeSimulation(): Unit= {
+    val simulation = new CloudSim()
+
+    val hostList1: List[Host] = IaasSimulation.createHostList()
+    val vmsList1: List[Vm] = IaasSimulation.createVmsList()
+    val cloudletList1: List[Cloudlet] = IaasSimulation.createCloudlets()
+    val dataCenter1 = new DatacenterSimple(simulation, hostList1.asJava, IaasSimulation.getTypeOfAllocation())
+    val schedulingInterval1 = config1.getInt("SCHEDULING_INTERVAL")
+    dataCenter1.setSchedulingInterval(schedulingInterval1)
+
+    val hostList2: List[Host] = SaasSimulation.createHostList()
+    val vmsList2: List[Vm] = SaasSimulation.createVmsList()
+    val cloudletList2: List[Cloudlet] = SaasSimulation.createCloudlets()
+    val dataCenter2 = new DatacenterSimple(simulation, hostList2.asJava, SaasSimulation.getTypeOfAllocation())
+    val schedulingInterval2 = config2.getInt("SCHEDULING_INTERVAL")
+    dataCenter2.setSchedulingInterval(schedulingInterval2)
+
+
+    val hostList3: List[Host] = PaasSimulation.createHostList()
+    val vmsList3: List[Vm] = PaasSimulation.createVmsList()
+    val cloudletList3: List[Cloudlet] = PaasSimulation.createCloudlets()
+    val dataCenter3 = new DatacenterSimple(simulation, hostList3.asJava, PaasSimulation.getTypeOfAllocation())
+    val schedulingInterval3 = config3.getInt("SCHEDULING_INTERVAL")
+    dataCenter3.setSchedulingInterval(schedulingInterval3)
+
+
+    val broker = new DatacenterBrokerSimple(simulation)
+
+    val vmlists = List(vmsList1, vmsList2, vmsList3)
+    val cloudletList = List(cloudletList1, cloudletList2, cloudletList3)
+    
+    broker.submitVmList((vmlists.flatMap(_.zipWithIndex).sortBy(_._2).map(_._1)).asJava)
+    broker.submitCloudletList((cloudletList.flatMap(_.zipWithIndex).sortBy(_._2).map(_._1)).asJava)
+
+    val networkTopology = new BriteNetworkTopology()
+    simulation.setNetworkTopology(networkTopology)
+    networkTopology.addLink(dataCenter1, broker, mainConfig.getDouble("NETWORK_BW"), mainConfig.getDouble("NETWORK_LATENCY"))
+    networkTopology.addLink(dataCenter2, broker, mainConfig.getDouble("NETWORK_BW"), mainConfig.getDouble("NETWORK_LATENCY"))
+    networkTopology.addLink(dataCenter3, broker, mainConfig.getDouble("NETWORK_BW"), mainConfig.getDouble("NETWORK_LATENCY"))
+
+
+    simulation.start()
+
+    val finishedCloudlets = broker.getCloudletFinishedList()
+    finishedCloudlets.sort(Comparator.comparingLong((cloudlet: Cloudlet) => cloudlet.getVm.getId))
+    new CloudletsTableBuilder(finishedCloudlets).build()
+
+  }
+}
+
+class CombinedSimulation
---
+++ src/main/scala/ModelSimulations/SaasSimulation.scala
@@ -0,0 +1,136 @@
+package ModelSimulations
+
+import HelperUtils.CreateLogger
+import com.typesafe.config.{Config, ConfigFactory}
+import org.cloudbus.cloudsim.allocationpolicies.{VmAllocationPolicy, VmAllocationPolicyBestFit, VmAllocationPolicyFirstFit, VmAllocationPolicyRoundRobin, VmAllocationPolicySimple}
+import org.cloudbus.cloudsim.brokers.DatacenterBrokerSimple
+import org.cloudbus.cloudsim.cloudlets.{Cloudlet, CloudletSimple}
+import org.cloudbus.cloudsim.core.CloudSim
+import org.cloudbus.cloudsim.datacenters.DatacenterSimple
+import org.cloudbus.cloudsim.hosts.{Host, HostSimple}
+import org.cloudbus.cloudsim.resources.{Pe, PeSimple}
+import org.cloudbus.cloudsim.schedulers.cloudlet.{CloudletSchedulerSpaceShared, CloudletSchedulerTimeShared}
+import org.cloudbus.cloudsim.schedulers.vm.{VmSchedulerSpaceShared, VmSchedulerTimeShared}
+import org.cloudbus.cloudsim.utilizationmodels.{UtilizationModelDynamic, UtilizationModelFull}
+import org.cloudbus.cloudsim.vms.{Vm, VmSimple}
+import org.cloudsimplus.builders.tables.CloudletsTableBuilder
+import org.slf4j.Logger
+
+import java.util
+import java.util.Comparator
+import java.util.function.Consumer
+import scala.util.Random
+import scala.jdk.CollectionConverters.*
+
+
+object SaasSimulation {
+  val logger: Logger = CreateLogger(classOf[SaasSimulation])
+  val config: Config = ConfigFactory.load("Saas.conf").getConfig("Saas")
+
+  def main(args: Array[String]): Unit = {
+    executeSimulation()
+  }
+
+  def createHostList(): List[Host] = {
+    val hostConfig = config.getConfigList("HOSTS")
+    val numberOfHosts = config.getInt("HOSTS_COUNT")
+    logger.info("The number of hosts" + numberOfHosts)
+    val hosts = (0 until numberOfHosts).map(index => {
+      val typeOfHost = Random.between(0, hostConfig.size())
+      val hostConfigVal = hostConfig.get(typeOfHost)
+      val HOST_PES = hostConfigVal.getInt("PES")
+
+      logger.info("The type of host" + typeOfHost)
+      logger.info("The hosts values:" + hostConfigVal)
+
+      val peList = new util.ArrayList[Pe](hostConfigVal.getInt("PES"))
+      (0 to HOST_PES - 1).map((index) => {
+        peList.add(new PeSimple(hostConfigVal.getInt("MIPS")))
+      })
+      val ram: Long = hostConfigVal.getInt("RAM")
+      val bw: Long = hostConfigVal.getInt("BDW")
+      val storage: Long = hostConfigVal.getInt("STORAGE")
+
+      val host = new HostSimple(ram, bw, storage, peList, false)
+      hostConfigVal.getString("VM_SCHEDULER") match
+        case "Time" => host.setVmScheduler(new VmSchedulerTimeShared())
+        case "Space" => host.setVmScheduler(new VmSchedulerSpaceShared())
+      host
+    }).toList
+    hosts
+  }
+
+  def getTypeOfAllocation(): VmAllocationPolicy = {
+    val alloactionPolicy = config.getString("ALLOCATION_POLICY")
+    alloactionPolicy match {
+      case "ROUND_ROBIN" => new VmAllocationPolicyRoundRobin()
+      case "SIMPLE" => new VmAllocationPolicySimple()
+      case "BEST_FIT" => new VmAllocationPolicyBestFit()
+      case "FIRST_FIT" => new VmAllocationPolicyFirstFit()
+    }
+  }
+
+  def getCloudletSchedularType(typeVal: String) = {
+    typeVal match {
+      case "Time" => new CloudletSchedulerTimeShared()
+      case "Space" => new CloudletSchedulerSpaceShared()
+    }
+  }
+
+  def createVmsList(): List[Vm] = {
+    val vmsConfig = config.getConfigList("VMS")
+    val noOfVms = config.getInt("VMS_COUNT")
+    val vms = (0 to noOfVms - 1).map(index => {
+      val typeOfVm = Random.between(0, vmsConfig.size())
+      val vmConfigVal = vmsConfig.get(typeOfVm)
+
+      logger.info("The type of host" + typeOfVm)
+      logger.info("The hosts values:" + vmConfigVal)
+
+      val vm = new VmSimple(index, 1000, vmConfigVal.getInt("VM_PES"))
+        .setRam(vmConfigVal.getInt("RAM"))
+        .setBw(vmConfigVal.getInt("BDW"))
+        .setSize(vmConfigVal.getInt("SIZE"))
+        .setCloudletScheduler(getCloudletSchedularType(vmConfigVal.getString("CLOUDLET_SCHEDULER")))
+      vm
+    }).toList
+    vms
+  }
+
+  def createCloudlets(): List[Cloudlet] = {
+    val cloudletConfig = config.getConfigList("CLOUDLETS")
+    val noOfCloudlets = config.getInt("CLOUDLETS_COUNT") + config.getInt("CLOUDLETS_REQUEST_COUNT") 
+    val cloudlets = (0 to noOfCloudlets - 1).map(index => {
+      val typeOfCloudlet = Random.between(0, cloudletConfig.size())
+      val cloudletConfigVal = cloudletConfig.get(typeOfCloudlet)
+      val utilization: UtilizationModelDynamic = new UtilizationModelDynamic(0.2)
+      val cloudlet: Cloudlet = new CloudletSimple(index, cloudletConfigVal.getInt("LENGTH"), cloudletConfigVal.getInt("PES"))
+        .setFileSize(cloudletConfigVal.getInt("SIZE"))
+        .setUtilizationModelCpu(new UtilizationModelFull).setUtilizationModelRam(utilization).setUtilizationModelBw(utilization)
+      cloudlet
+    }).toList
+    cloudlets
+  }
+  def executeSimulation(): Unit = {
+    val simulation = new CloudSim()
+    val hostList: List[Host] = createHostList()
+    val vmsList: List[Vm] = createVmsList()
+    val cloudletList: List[Cloudlet] = createCloudlets()
+    val dataCenter = new DatacenterSimple(simulation, hostList.asJava, getTypeOfAllocation())
+    val schedulingInterval = config.getInt("SCHEDULING_INTERVAL")
+    dataCenter.setSchedulingInterval(schedulingInterval)
+
+    val broker = new DatacenterBrokerSimple(simulation)
+
+    broker.submitVmList(vmsList.asJava)
+    broker.submitCloudletList(cloudletList.asJava)
+
+    simulation.start()
+
+    val finishedCloudlets = broker.getCloudletFinishedList()
+    finishedCloudlets.sort(Comparator.comparingLong((cloudlet: Cloudlet) => cloudlet.getVm.getId))
+    new CloudletsTableBuilder(finishedCloudlets).build()
+  }
+}
+
+class SaasSimulation
